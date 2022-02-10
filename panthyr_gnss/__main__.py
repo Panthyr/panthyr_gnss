69064daad8ed5f94a064477d380301d9ab216e8e
dietervansteenwegen <github@vansteenwegen.org>
2022-02-10 21:29:52 +0100

    pre-commit hook run

--- panthyr_gnss/__main__.py
+++ panthyr_gnss/__main__.py
@@ -1,12 +1,11 @@
 #! /usr/bin/python3
-# coding: utf-8
+# -*- coding: utf-8 -*-
 # Module: __main__.py
 # Authors: Dieter Vansteenwegen
-# Institution: VLIZ (Vlaams Institute voor de Zee)
+# Institution: VLIZ (Vlaams Instituut voor de Zee)
 
 __author__ = 'Dieter Vansteenwegen'
 __version__ = '0.1b'
-__credits__ = 'Dieter Vansteenwegen'
 __email__ = 'dieter.vansteenwegen@vliz.be'
 __status__ = 'Development'
 __project__ = 'Panthyr'
